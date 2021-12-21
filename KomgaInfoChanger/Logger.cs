cc11fc1dff418ba1c182f4b1e8071616f342232f
Kyaru <qn1213@naver.com>
2021-12-21 14:42:05 +0900

    [Refactor] 로그 파일 경로 수정 및 로그 큐 호출 메서드 수정

--- KomgaInfoChanger/Logger.cs
+++ KomgaInfoChanger/Logger.cs
@@ -5,38 +5,28 @@ namespace KomgaInfoChanger
 {
     internal class Logger
     {
-        private QueueFactory<string> loggers;        
+        private QueueFactory<string> loggers;
+        private string filePath = env.logPath + Helper.GetDate() + ".txt";
 
         public Logger()
         {
-            // 호출 후 로그 파일 생성(날짜별)
-            if(!File.Exists(env.logPath))
+            if (!File.Exists(filePath))
             {
-                using(File.Create(env.logPath)){}
+                using(File.Create(filePath)){}
             }
 
             loggers = new QueueFactory<string>();
             loggers.DoingJob += Log;
         }
 
-        public void AddLog(object sender, string e)
+        public void AddLog(string e)
         {
             loggers.Enqueue(e);
         }
 
         private void Log(object sender, QueueThreadEventArgs<string> e)
-        {            
-            try
-            {
-                StreamWriter writer = null;
-                writer = File.AppendText(env.logPath);
-                writer.WriteLine(e);
-                writer.Close();
-            }
-            catch (Exception)
-            {
-                loggers.Stop(); // 여기까지 오면 우짜지... 방법 제안좀요 ㅠ
-            }
+        {
+            File.AppendAllText(filePath, e.TypeArgs);
         }
     }
 }
