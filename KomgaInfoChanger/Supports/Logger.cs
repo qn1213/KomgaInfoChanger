c76d87f8af0498b5d49d9cde79f448131b0d17d1
Kyaru <qn1213@naver.com>
2021-12-22 12:37:17 +0900

    [Refactor] 로그클래스 싱글톤 적용 및 동기화 수정

--- KomgaInfoChanger/Supports/Logger.cs
+++ KomgaInfoChanger/Supports/Logger.cs
@@ -1,43 +1,53 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.IO;
 
 namespace KomgaInfoChanger
 {
     // 로그는 실패한 기록만 남길 것.
-    // 그외엔 자원 아까움
     internal class Logger
     {
+        private static readonly Lazy<Logger> instance = new Lazy<Logger>(() => new Logger());
+
         private QueueFactory<string> loggers;
-        private StringBuilder builder;
 
         private string filePath = env.logPath + Helper.GetDate() + "_InfoChangerLog.txt";
 
-        public Logger()
+        private readonly object _fileLock = new object();
+
+        private Logger()
         {
             if (!File.Exists(filePath))
-            {
-                using(File.Create(filePath)){}
-            }
+                using (File.Create(filePath)) { }
 
             loggers = new QueueFactory<string>();
             loggers.DoingJob += Log;
+        }
 
-            builder = new StringBuilder();
+        public static Logger GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
         }
 
         public void AddLog(string e)
-        {            
+        {
             loggers.Enqueue(e);
         }
 
         private void Log(object sender, QueueThreadEventArgs<string> e)
         {
+            StringBuilder builder = new StringBuilder();
             builder.Append("[");
             builder.Append(Helper.GetTime());
             builder.Append("] ");
             builder.Append(e.TypeArgs);
+            builder.Append("\r\n");
 
-            File.AppendAllText(filePath, e.TypeArgs);
+            lock (_fileLock)
+                File.AppendAllText(filePath, builder.ToString());
         }
     }
 }
