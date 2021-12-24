aa96426c17e31c9b479ee13660e8edda25dc892a
Kyaru <qn1213@naver.com>
2021-12-24 17:27:34 +0900

    [Feature] Zip파일 예제 추가

--- KomgaInfoChanger/App.xaml.cs
+++ KomgaInfoChanger/App.xaml.cs
@@ -12,20 +12,36 @@ namespace KomgaInfoChanger
             Logger log = Logger.GetInstance;
 
             env.info.serverAddr = "https://naver.com";
-            env.info.serverID = "1234@naver.com";
-            env.info.serverPW = "1234";
+            env.info.serverID = "a@naver.com";
+            env.info.serverPW = "a!";
 
-            Protocols.ReqSetCookie req = new Protocols.ReqSetCookie();
+            //Protocols.ReqSetCookie req = new Protocols.ReqSetCookie();
 
-            if(!req.Request())
-                log.AddLog(req.res.status + " : " + req.res.error + " :" + req.res.message);
-            else
-                log.AddLog("로그인 성공");
+            //if(!req.Request())
+            //    log.AddLog(req.res.status + " : " + req.res.error + " :" + req.res.message);
+            //else
+            //    log.AddLog("로그인 성공");
 
-            Protocols.ReqBooksInfo req2 = new Protocols.ReqBooksInfo();
-            ConcurrentDictionary<string, Infos.SAttribute> tmp = req2.Request();
+            //Protocols.ReqBooksInfo req2 = new Protocols.ReqBooksInfo(2);
+            //ConcurrentDictionary<string, SBookAttribute> tmp = req2.Request();
 
+            //foreach(var item in tmp)
+            //{
+            //    log.AddLog(item.Key + " : " + item.Value.id + ", " + item.Value.seriesId + ", " + item.Value.libraryId);
+            //}
+
+
+            ArchiveLoader loader = new ArchiveLoader();
+            SMetaDataAttribute temp = loader.GetInfoFromFile(@"C:\SMB\test.zip");
+            string authors = string.Join(",", temp.authors);
+            string tags = string.Join (",", temp.tags);
+
+            log.AddLog(temp.number + ", " + temp.title + ", " + authors + ", " + tags);
+            
 #else
+
+
+
             Window login = new LoginWindow();
             login.Show();
 
