7d26e41815d62ab4a9fc18fdf9dd9bf3c2a37f94
Kyaru <qn1213@naver.com>
2021-12-24 17:24:51 +0900

    [Feature] Zip파일 메타데이터 구조체 추가

---
+++ KomgaInfoChanger/Infos/Attributes.cs
@@ -0,0 +1,18 @@
+﻿namespace KomgaInfoChanger
+{    public struct SBookAttribute
+    {
+        public string id;           // => [id]
+        public string seriesId;     // => [seriesId]
+        public string libraryId;    // => [libraryId]
+        //public string name;         // => [name]
+        public string mediaType;    // => [media][mediaType]
+    }
+
+    public struct SMetaDataAttribute
+    {
+        public string number;   // => 갤러리 넘버
+        public string title;    // => 제목
+        public string[] authors;// => 작가(2명 이상 참여작일 때), Role은 Artist로 하드코딩할 예정
+        public string[] tags;   // => 태그
+    }
+}
